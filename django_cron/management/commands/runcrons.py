f2a4d32ebba3f4ad446cf7c1d45c39274234392e
Sam Gillespie <42567001+sgillespiefuncaptcha@users.noreply.github.com>
2020-03-25 13:27:06 +1000

    Update runcrons.py

--- django_cron/management/commands/runcrons.py
+++ django_cron/management/commands/runcrons.py
@@ -42,10 +42,10 @@ class Command(BaseCommand):
 
         try:
             crons_to_run = [get_class(x) for x in cron_class_names]
-        except Exception:
+        except Exception as e:
             error = traceback.format_exc()
             self.stdout.write('Make sure these are valid cron class names: %s\n%s' % (cron_class_names, error))
-            return
+            raise e
 
         for cron_class in crons_to_run:
             run_cron_with_cache_check(
